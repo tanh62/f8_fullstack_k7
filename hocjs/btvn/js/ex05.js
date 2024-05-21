1e331b6115ad186c1fe4c79fa31589497ada867e
tanh62 <tanh0602.ty1@gmail.com>
2024-05-22 00:05:44 +0700

    bt

--- hocjs/btvn/js/ex05.js
+++ hocjs/btvn/js/ex05.js
@@ -33,4 +33,38 @@ console.log(getError("password"));
 //B2
 
 //B3:
+function User(name, password, email) {
+    this.name = name;
+    this.password = password;
+    this.email = email;
+}
+var data = [];
+function handelRegister(name, password, email) {
+    if (!name || !password || !email) {
+        return console.log("Xin hãy nhập đủ thông tin");
+    }
+    var newData = new User(name, password, email);
+    newData.role = "user";
+    data.push(newData);
+    return data;
+}
+var dataRegister = handelRegister(
+    "Nguyen Van A",
+    "123456",
+    "nguyenvana@email.com"
+);
+var dataRegister = handelRegister(
+    "Nguyen Van B",
+    "123456",
+    "nguyenvanb@email.com"
+);
+function handleLogin(email, password) {
+    for (var element of data) {
+        if (element["email"] === email && element["password"] === password)
+            return element;
+    }
+}
+console.log(data);
+console.log(handleLogin("nguyenvana@email.com", "123456"));
+console.log(handleLogin("nguyenvana@email.com", "12345678"));
 
