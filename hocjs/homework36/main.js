3d5c2878e2e9b0fa7f6e7eba8c9f00f064734dfb
tanh62 <tanh0602.ty1@gmail.com>
2024-07-02 23:25:41 +0700

    bai36

---
+++ hocjs/homework36/main.js
@@ -0,0 +1,46 @@
+
+const title_link = document.querySelector(".title_link");
+let title = title_link.innerText;
+// console.log(title);
+title_link.innerText = title.toUpperCase();
+let button = document.querySelector("button");
+button.disabled = true;
+button.addEventListener("click", () => {
+    if (count == 0) {
+        window.location.href = "";
+    }
+});
+document.addEventListener("keydown", (e) => {
+    e.preventDefault();
+});
+// document.addEventListener("contextmenu", function (event) {
+//     event.preventDefault(); // Ngăn chặn hành động mặc định của trình duyệt
+// });
+let count = 30;
+const counterElement = document.querySelector(".count");
+console.log(counterElement);
+const decrement = 1;
+const interval = 1000;
+let lastTime = performance.now();
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+        cancelAnimationFrame(animationFrameId);
+    } else {
+        requestAnimationFrame(updateCounter);
+    }
+});
+
+let animationFrameId;
+function updateCounter(currentTime) {
+    if (count > 0) {
+        if (currentTime - lastTime >= interval) {
+            count -= decrement;
+            counterElement.textContent = count;
+            lastTime = currentTime;
+        }
+        requestAnimationFrame(updateCounter);
+    } else {
+        button.disabled = false;
+    }
+}
+animationFrameId = requestAnimationFrame(updateCounter);
